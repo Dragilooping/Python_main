e06e3fed698410fc3f987cfd1c8d72cbf2e02050
Valier-App <clementport28@gmail.com>
2024-12-06 18:46:07 +0100

    Update NL_Rankings_Selenium.py

--- NL_Rankings_Selenium.py
+++ NL_Rankings_Selenium.py
@@ -1,12 +1,12 @@
-from datetime import datetime
-import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 import csv
+import time
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
